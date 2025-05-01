c81c8bf490304566c60b9956d3f806c2e98e1396
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-05-01 15:02:46 +0000

    All done? Added two paths to routes with a form and a post command to the database

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,26 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def input
+
+    render({ :template => "item_templates/backdoor" })
+  end
+
+  def insert
+    @linkurl = params[:query_link_url]
+    @linkdes = params[:query_link_description]
+    @thumburl = params[:query_thumbnail_url]
+
+    # Create a new item
+    new_item = Item.new
+    new_item.link_url = @linkurl
+    new_item.link_description = @linkdes
+    new_item.thumbnail_url = @thumburl
+    new_item.save
+
+    # includes a redirect to homepage
+    redirect_to("/")
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", { :controller => "items", :action => "input" })
+
+  post("/insert_item", { :controller => "items", :action => "insert" })
+
 end
